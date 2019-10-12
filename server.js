4358534a5a24a71b8abd0556626a7d5d6e281192
Emanuele <emanueca@stud.ntnu.no>
2019-10-12 16:28:37 +0200

    Issue #5: added put method in API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require("fs");
 var path = require("path");
 app.use(express.static("data"));
 
@@ -17,3 +18,28 @@ app.get("/api/feature-collection/:id/json", (req, res) => {
 app.get("/api/feature-collection/total/json/", (req, res) => {
   res.sendFile(path.join(__dirname, "./data/feature-collection/total.json"));
 });
+//Put endpoint for updating feature collection. Request with 2 params, ID for feat.coll. ID and DATA for feat.coll. Data to write on File
+app.put("/api/feature-collection/:id/", (req, res) => {
+  let feature_collection = req.params.feature_collection;
+  if (isFeatureCollectionValid(feature_collection)) {
+    //Update the feature collection object
+    fs.writeFileSync(
+      path.join(
+        __dirname,
+        "./data/feature-collection/",
+        req.params.id + ".json"
+      ),
+      JSON.stringify(feature_collection)
+    );
+  } else {
+    //Throw an error
+    console.log(
+      "The input to the API is not a valid feature collection object"
+    );
+  }
+});
+
+function isFeatureCollectionValid(data) {
+  //Given a feature collection object, returns true if is a valid feature collection.
+  return true;
+}
